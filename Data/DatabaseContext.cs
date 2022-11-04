3214e4ca9ce70217f0a49960714e7e2047f9d302
24wings <731502029@qq.com>
2022-11-04 23:04:13 +0800

    first

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SyncfusionBlazorApp1.Data
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {
+
+         }
+       
+        public DbSet<User> Users { get; set; }
+    }
+}
